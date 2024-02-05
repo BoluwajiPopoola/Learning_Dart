6ef847b5f8aaa66605410d57b7f38aca7bfccd33
Boluwaji Popoola <Moboluwaji41@gmail.com>
2024-02-05 16:41:44 +0100

    Introduction to learning about variable. I would definitely delve deeper after I complete this intro course

---
+++ introduction_to_dart/bin/variable.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Boluwaji";
+  String address = "Lagos, Nigeria";
+  int age = 55;
+  double height = 5.9;
+  bool isMale = true;
+
+  print("Hi! My name is $name,");
+  print("I live in $address,");
+  print("I am not $age years old,");
+  print("Neither am I $height feet tall.");
+  print("But I am a male $isMale");
+}
