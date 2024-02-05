da37c148b79fe35547d344ac854309f88176ae92
Boluwaji Popoola <Moboluwaji41@gmail.com>
2024-02-05 17:56:50 +0100

    I generated a few questions to test my knowledge on the basic dart types. Here are the solutions.

---
+++ introduction_to_dart/bin/datatype_practice.dart
@@ -0,0 +1,35 @@
+void main() {
+  /**
+   * STRING MANIPULATION
+   * Declare a string variable named "greeting" with the value "Hello, Dart!".
+   * Create a new string variable that concatenates "greeting" with your name.
+   */
+
+  String greeting = "Hello, Dart!";
+  print("$greeting my name is Boluwaji");
+
+  /**
+   * NUMERIC OPERATIONS
+   * Declare two integer variables, "num1" and "num2", with values 10 and 5 respectively.
+   * Calculate the sum, difference, product and quotient of "num1" and "num2"
+   */
+
+  int num1 = 10;
+  int num2 = 5;
+
+  print("The sum is $num1 + $num2");
+  print("The difference is $num1 - $num2");
+  print("The product is $num1 * $num2");
+  print("The quotent is $num1 / $num2");
+
+  /**
+   * BOOLEAN LOGIC
+   * Create a boolean variable named isDartFun and set it to true.
+   * Write a statement that prints "Dart is fun!" if isDartFun is true.
+   */
+  bool isDartFun = true;
+  
+  if (isDartFun) {
+    print("Dart is fun!");
+  } 
+}
