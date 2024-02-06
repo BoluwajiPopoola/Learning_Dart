13a4bed635d95347741a68bcef2b62da738644eb
Boluwaji Popoola <Moboluwaji41@gmail.com>
2024-02-06 22:29:42 +0100

    More Questions for practice on introduction to dart

---
+++ introduction_to_dart/bin/questionsforpractice1.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  // Write a program to print your name in Dart
+  print("My name is Boluwaji");
+  //Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+  print("Hello I am \"John Doe\"");
+  print("Hello I'am \"John Doe\"");
+  //Declare constant type of int set value 7
+  const num = 7;
+  print(num);
+  //Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+
+  //Write a program to print a square of a number using user input.
+  print("Enter number:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  int square = num1 * num1;
+  print("The Square of the number you entered is $square");
+  //Write a program to print full name of a from first name and last name using user input.
+  print("Enter First Name:");
+  String? firstName = stdin.readLineSync();
+  print("Enter Last Name:");
+  String? lastName = stdin.readLineSync();
+  print("Your full name is $firstName $lastName");
+  //Write a program to find quotient and remainder of two integers
+
+  //Write a program to find quotient and remainder of two integers
+  
+
+}
